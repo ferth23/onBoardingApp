960fdb599082122d19d2054cb0d12e41b3b0ef9e
ferth23 <torresherrerafernanda1@gmail.com>
2024-11-13 13:36:56 -0600

    first commit

---
+++ app/src/main/java/com/example/boardingapp/viewModel/MainViewBoarding.kt
@@ -0,0 +1,52 @@
+package com.example.boardingapp.viewModel
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+import com.example.boardingapp.R
+import com.example.boardingapp.data.PageData
+import com.example.boardingapp.dataStore.StoreBoarding
+import com.example.boardingapp.onBoardViews.OnBoardingPager
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.rememberPagerState
+import java.util.ArrayList
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun MainViewBoarding ( navController: NavController, store: StoreBoarding ) {
+    val items = ArrayList<PageData>()
+
+    items.add (
+        PageData ( R.raw.page1, "Bienvenidos", "Bienvenidos a la aplicación para mostrar el uso del onBoarding " )
+    )
+
+    items.add (
+        PageData ( R.raw.page2, "Uso de onBoarding", "Esta es la segunda página del Boarding mostrando una descripción" )
+    )
+
+    items.add (
+        PageData ( R.raw.page3, "Fin de Boarding", "Esta es la página final del Boarding y ya debe aparecer el botón" )
+    )
+
+    val pagerState = rememberPagerState (
+        pageCount = items.size,
+        initialOffscreenLimit = 2,
+        infiniteLoop = false,
+        initialPage = 0
+    )
+
+    OnBoardingPager (
+        item = items,
+        pagerState = pagerState,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background ( Color.White ),
+        navController,
+        store
+    )
+}
